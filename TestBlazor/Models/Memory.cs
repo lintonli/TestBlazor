93176bb159dd658b79309755f6475df0daf01cff
linton <lintonli162@gmail.com>
2024-01-09 15:01:02 +0300

    Add project files.

---
+++ TestBlazor/Models/Memory.cs
@@ -0,0 +1,11 @@
+﻿namespace TestBlazor.Models
+{
+    public class Memory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Location { get; set; } = string.Empty;
+    }
+}
